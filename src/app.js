e4418475809c9d75c050ffef90d437c5447eb7be
Simon <simon.joachim@qemotion.com>
2020-04-21 12:35:35 +0200

    rmv dummy expense data

--- src/app.js
+++ src/app.js
@@ -13,13 +13,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 
 const store = configureStore();
-store.dispatch(addExpense({description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({description: 'Rent', amount: 41500 }));
-
-store.dispatch(addExpense({description: 'Gas bill'}));
-
-
-
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
